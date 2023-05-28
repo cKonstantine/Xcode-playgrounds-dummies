734e652f888a62d837e3d14a8837339a9086f4b3
Constantin Mindruta <constantin.mindruta@gmail.com>
2023-05-28 16:13:08 +0300

    First commit ever made

---
+++ TextEditor/TextEditor/TextEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextEditorApp.swift
+//  TextEditor
+//
+//  Created by TestMac on 14/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TextEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
